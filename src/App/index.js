cf8c18966a18a6250b6228e548f89a130885af4d
matt <mpocock@me.com>
2018-07-06 21:08:06 +0100

    Added prettier and prettified my files

--- src/App/index.js
+++ src/App/index.js
@@ -3,16 +3,8 @@ import styles from './index.scss';
 
 const App = () => (
     <div>
-        <h1
-            className={styles.green}
-        >
-            Hello World
-        </h1>
-        <h3
-            className={styles.red}
-        >
-            Now with CSS Modules!
-        </h3>
+        <h1 className={styles.green}>Hello World</h1>
+        <h3 className={styles.red}>Now with CSS Modules!</h3>
     </div>
 );
 
--- src/App/tests/index.spec.js
+++ src/App/tests/index.spec.js
@@ -5,9 +5,7 @@ import { expect } from 'chai';
 import App from '../index';
 
 test('Should render a Hello World', () => {
-    const wrapper = mount(
-        <App />
-    );
+    const wrapper = mount(<App />);
 
     expect(wrapper.find('div')).to.have.length(1);
 });
